aee30d8bd403ac7a340a844a12ca1044302612c9
salsabilarhdsy <salsabilarhdsy@gmail.com>
2018-03-15 09:29:46 +0700

    5th commit

--- resources/views/list_products.blade.php
+++ resources/views/list_products.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Kasir</title>
+  <title>Koperasi Siswa</title>
   <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/bootstrap-3.3.5/css/bootstrap.min.css">
